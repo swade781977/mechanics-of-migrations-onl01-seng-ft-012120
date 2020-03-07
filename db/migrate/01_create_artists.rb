66661272ebe4016be9bb0cfed19310ac554b5be0
Chris Wade <swade781977@yahoo.com>
2020-03-07 21:12:32 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -7,6 +7,10 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   
   def change
     create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.interger :age
+      t.string :hometown
     end
   end
 end
